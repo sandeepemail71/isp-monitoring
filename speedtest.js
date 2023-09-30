e22944994a5405cbf204b4f329cb735cbf652336
sandeep kumar <sandeepemail71@gmail.com>
2023-09-30 21:57:51 +0530

    api call is added in speedtest script

--- speedtest.js
+++ speedtest.js
@@ -1,7 +1,7 @@
 const axios = require("axios");
 const { exec } = require("child_process");
 
-const apiUrl = "https://isp-monitoring-48ee36e4594e.herokuapp.com/monitor";
+const apiUrl = "https://isp-monitoring-48ee36e4594e.herokuapp.com/speedtest";
 
 const commandToRun = "speedtest-cli --json";
 
@@ -25,6 +25,24 @@ exec(commandToRun, (error, stdout, stderr) => {
     console.log("Speed Test Results (Mbps):");
     console.log(`Download speed: ${downloadSpeedMbps.toFixed(2)} Mbps`);
     console.log(`Upload speed: ${uploadSpeedMbps.toFixed(2)} Mbps`);
+    const postData = {
+      timeStamp: Date.now(),
+      clientName: "BLR_HOME",
+      uploadSpeed: uploadSpeedMbps.toFixed(2),
+      downloadSpeed: downloadSpeedMbps.toFixed(2),
+    };
+
+    axios
+      .post(apiUrl, postData)
+      .then((response) => {
+        // Handle the response data here
+        console.log("Response Data:");
+        console.log(response.data);
+      })
+      .catch((error) => {
+        // Handle any errors that occurred during the request
+        console.error("Error:", error.message);
+      });
   } catch (parseError) {
     console.error("Error parsing JSON:", parseError.message);
   }
